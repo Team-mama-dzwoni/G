8a87ab9a3cc0890ec73cd6f27152cb1206e9f712
Tytus <podejmatytus@gmail.com>
2026-01-17 20:08:35 +0100

    komentarze i odpowiedzi

--- posts/posts-API.js
+++ posts/posts-API.js
@@ -13,25 +13,29 @@ Promise.all([
                 <h2>${post.content}</h2>
                 <p>(Placeholder)</p>
             </div>
-            <div id="comments">`
+            <div class="comments">`
             // Creates comments
             comments.forEach(comment => {
                 if (comment.post_id === post.id) {
                     if (comment.parent_id == null) {
                         postsHTML.innerHTML += `
                         <div class="comment">
-                            <div class="pfp"></div>
-                            <h3>${comment.nickname}</h3>
+                            <div class="person">
+                                <div class="pfp"></div>
+                                <h3>${comment.nickname}</h3>
+                            </div>
                             <p>${comment.content}</p>
                         </div>`
-                        postsHTML.innerHTML += `<div id="Comment-replies">`;
+                        postsHTML.innerHTML += `<div class="Comment-replies">`;
                         // Replies for comments
                         comments.forEach(reply => {
                             if (reply.parent_id === comment.id) {
                                 postsHTML.innerHTML += `
                                     <div class="comment-reply">
-                                        <div class="pfp"></div>
-                                        <h3 style="color:red">${reply.nickname}</h3>
+                                        <div class="person">
+                                            <div class="pfp"></div>
+                                            <h3 style="color:red">${reply.nickname}</h3>
+                                        </div>
                                         <p>${reply.content}</p>
                                     </div>`
 
